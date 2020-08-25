28659abc4169c2125681220d24fcac7b4866efb1
r31oad3d <r31oad3d@163.com>
2020-08-25 22:40:19 +0800

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+This is simple template that uses Yew to build frontend.
+-------
+
++ first, you need:
+> yarn install
+
++ build for production:
+> yarn run build
+
++ for local development:
+> yarn run start:dev
---
+++ src/lib.rs
@@ -0,0 +1,56 @@
+#![recursion_limit = "512"]
+use wasm_bindgen::prelude::*;
+use yew::prelude::*;
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+struct Model {
+    link: ComponentLink<Self>,
+    value: i64,
+}
+
+enum Msg {
+    AddOne,
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Self { link, value: 0 }
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::AddOne => self.value += 1,
+        }
+        true
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        // Should only return "true" if new properties are different to
+        // previously received properties.
+        // This component has no properties so we will always return "false".
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <div>
+                <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
+                <p>{ self.value }</p>
+            </div>
+        }
+    }
+}
+
+// This is the entry point for the web app
+#[wasm_bindgen]
+pub fn run_app() -> Result<(), JsValue> {
+    wasm_logger::init(wasm_logger::Config::default());
+    yew::start_app::<Model>();
+    Ok(())
+}
